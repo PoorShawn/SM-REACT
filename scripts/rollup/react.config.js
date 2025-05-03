79931f99b2f7dd5c52dc1b6e564af13601d28823
PoorShawn <18352979520@163.com>
2025-05-03 22:33:36 +0800

    feat: implement JSX transform to ReactElement

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,54 @@
+import { getBaseRollupPlugins, getPackageJSON, resolvePkgPath } from "./utils";
+import generatePackageJson from "rollup-plugin-generate-package-json";
+
+const { name, module } = getPackageJSON("react");
+
+// react 包的路径
+const pkgPath = resolvePkgPath(name);
+
+// react 打包后产物的路径
+const distPath = resolvePkgPath(name, true);
+
+export default [
+  // react
+  {
+    input: `${pkgPath}/${module}`,
+    output: {
+      file: `${distPath}/index.js`,
+      name: 'react',
+      format: "umd",
+    },
+    plugins: [
+      ...getBaseRollupPlugins(),
+      generatePackageJson({
+        inputFolder: pkgPath,
+        outputFolder: distPath,
+        baseContents: ({ name, description, version }) => ({
+          name,
+          description,
+          version,
+          main: 'index.js'
+        })
+      })
+    ]
+  },
+  // jsx-runtime
+  {
+    input: `${pkgPath}/src/jsx.ts`,
+    output: [
+      // jsx-runtime
+      {
+        file: `${distPath}/jsx-runtime.js`,
+        name:  `jsx-runtime.js`,
+        format: 'umd',
+      },
+      // jsx-dev-runtime
+      {
+        file: `${distPath}/jsx-dev-runtime.js`,
+        name:  `jsx-dev-runtime.js`,
+        format: 'umd',
+      }
+    ],
+    plugins: getBaseRollupPlugins()
+  }
+]
---
+++ scripts/rollup/utils.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import fs from 'fs';
+
+import ts from 'rollup-plugin-typescript2';
+import cjs from '@rollup/plugin-commonjs';
+
+const pkgPath = path.resolve(__dirname, '../../packages');
+const distPath = path.resolve(__dirname, '../../dist/node_modules');
+
+// 根据包名，获取包的路径
+export function resolvePkgPath(pkgName, isDist) {
+  if (isDist) {
+    return `${distPath}/${pkgName}`;
+  }
+  return `${pkgPath}/${pkgName}`;
+}
+
+// 根据包名，获取包的 package.json 配置
+export function getPackageJSON(pkgName) {
+  const path = `${resolvePkgPath(pkgName)}/package.json`;
+  const str = fs.readFileSync(path, { encoding: 'utf-8' });
+  return JSON.parse(str);
+}
+
+// 获取基础的 rollup 插件
+export function getBaseRollupPlugins({ typescript = {}} = {}) {
+  return [cjs(), ts(typescript)];
+}
